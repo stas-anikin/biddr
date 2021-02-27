d5071e7625add4ab455d0df71b1c4f36faf7a7e1
Stas Anikin <72667971+stas-anikin@users.noreply.github.com>
2021-02-26 20:40:06 -0800

    Update auction.rb

--- biddr_api/app/models/auction.rb
+++ biddr_api/app/models/auction.rb
@@ -3,16 +3,10 @@ class Auction < ApplicationRecord
   belongs_to :user
   before_validation :set_default_reserve_price
 
-
-  # validates(:title, presence: true, uniqueness: true)
-  # validates :description,
-  #             presence: { message: "must exist"}, length: { minimum: 2 }
-
   scope :viewable, -> { 
     where(aasm_state: [:published, :reserve_met])
   }
 
-  #State machines
   include AASM
 
   aasm whiny_transitions: false do
@@ -32,9 +26,7 @@ class Auction < ApplicationRecord
   private
 
   def set_default_reserve_price
-    # If you are writing to an attribute accessor,
-    # you must prefix with self. which you do not
-    # have to do if you are just reading it insead
+
     self.reserve_price ||= 1
   end
 
